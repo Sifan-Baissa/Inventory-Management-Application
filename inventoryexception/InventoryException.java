3e34e21725faecf05b9cb463acb93e23b0761f5d
Sifan Baissa <120062106+Sifan-Baissa@users.noreply.github.com>
2025-06-21 05:44:30 -0400

    Add custom InventoryException class

---
+++ inventoryexception/InventoryException.java
@@ -0,0 +1,25 @@
+package inventoryexception;
+
+public class InventoryException extends Exception {
+    public InventoryException() {
+        super();
+    }
+
+    public InventoryException(String message) {
+        super(message);
+    }
+
+    public InventoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InventoryException(Throwable cause) {
+        super(cause);
+    }
+
+    public InventoryException(String message, Throwable cause,
+                              boolean enableSuppression,
+                              boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
